1cfce8766c4b9b2cfdd5dc753f2b96e114123f1c
sophearahsp <sophearahspsp@gmail.com>
2020-08-08 06:04:27 -0700

    bar chart for individual countries works

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data:{ confirmed, deaths, recovered}, country }) => {
     const [dailyData, setDailyData] = useState([]);
     
     useEffect(() => {
@@ -12,9 +12,7 @@ const Chart = () => {
             setDailyData (await fetchDailyData());
         }
 
-        console.log(dailyData);
         fetchAPI();
-        
     }, []);
 
     const lineChart = (
@@ -35,12 +33,31 @@ const Chart = () => {
                 }],
             }}
         />) : null
+    );
 
+    const barChart = (
+        confirmed ? (
+            <Bar
+                data={{
+                    labels: ['Infected','Recovered', 'Deaths'],
+                    datasets: [{
+                        label: 'People',
+                        backgroundColor: ['rgba(0,0,255,0.5)','rgba(0,255,0,0.5)','rgba(255,0,0,0.5)'],
+                        data: [confirmed.value, recovered.value, deaths.value]
+                    }]
+
+                }}
+                options={{
+                    legend: { display: false},
+                    title: { display: true, text:`Current state in ${country}`}
+                }}
+            />
+        ) : null
     );
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 
