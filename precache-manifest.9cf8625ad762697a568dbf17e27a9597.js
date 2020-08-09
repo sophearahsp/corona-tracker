07516728bc1c67c1a6f35f3c7150c98d4a14794a
sophearahsp <sophearahspsp@gmail.com>
2020-08-08 22:11:36 -0700

    Updates

---
+++ precache-manifest.9cf8625ad762697a568dbf17e27a9597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b586a293188e3d8ae86bd151b6ab87cc",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "ed6c8cbd953b6599318f",
+    "url": "/corona-tracker/static/css/main.511151fa.chunk.css"
+  },
+  {
+    "revision": "6ed0c8a3e4e416109518",
+    "url": "/corona-tracker/static/js/2.43d5e4ad.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.43d5e4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6c8cbd953b6599318f",
+    "url": "/corona-tracker/static/js/main.b5d33f80.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
